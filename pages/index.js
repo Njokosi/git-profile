cf8f8fbcf711b879c74cf35657052182d14a35ea
jones <eng.njokosi@gmail.com>
2021-07-13 14:28:00 +0300

    update data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Layout from '../layouts'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Layout from "../layouts";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Git profile</title>
-        <meta name="description" content="Beautified version of your github profile" />
+        <title>GitProfile</title>
+        <meta
+          name="description"
+          content="Beautified version of your github profile"
+        />
         <link rel="icon" href="/favicon.svg" />
       </Head>
 
       <Layout />
-
-      
-
-      
     </div>
-  )
+  );
 }
---
+++ pages/user.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import Head from "next/head";
+
+const User = (props) => {
+  //   const username = props.query.id;
+  const username = "njokosi";
+  const [userData, setUserData] = useState(null);
+  const [langData, setLangData] = useState(null);
+  const [repoData, setRepoData] = useState(null);
+  const [error, setError] = useState({ active: false, type: 200 });
+  const [rateLimit, setRateLimit] = useState(null);
+
+  const getUserData = () => {
+    fetch(`https://api.github.com/users/${username}`)
+      .then((response) => {
+        if (response.status === 404) {
+          return setError({ active: true, type: 404 });
+        }
+        if (response.status === 403) {
+          return setError({ active: true, type: 403 });
+        }
+        return response.json();
+      })
+      .then((json) => setUserData(json))
+      .catch((error) => {
+        setError({ active: true, type: 400 });
+        console.error("Error:", error);
+      });
+  };
+
+  const getRepoData = () => {
+    fetch(`https://api.github.com/users/${username}/repos?per_page=100`)
+      .then((response) => {
+        if (response.status === 404) {
+          return setError({ active: true, type: 404 });
+        }
+        if (response.status === 403) {
+          return setError({ active: true, type: 403 });
+        }
+        return response.json();
+      })
+      .then((json) => setRepoData(json))
+      .catch((error) => {
+        setError({ active: true, type: 200 });
+        console.error("Error:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetch(`https://api.github.com/rate_limit`)
+      .then((response) => response.json())
+      .then((json) => {
+        setRateLimit(json.resources.core);
+        if (json.resources.core.remaining < 1) {
+          setError({ active: true, type: 403 });
+        }
+      });
+
+    getUserData();
+    // getLangData();
+    getRepoData();
+
+    // setUserData(mockUserData);
+    // setLangData(mockLangData);
+    // setRepoData(mockRepoData);
+  }, []);
+
+  return (
+    <>
+      <Head title={`${username ? `GitProfile | ${username}` : "GitProfile"}`} />
+      <div>{console.log("The user data: ", userData)}</div>
+    </>
+  );
+};
+
+export default User;
