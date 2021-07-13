108f8307420e8ea68b0aa9bc526e4ce354cbfdb8
jones <eng.njokosi@gmail.com>
2021-07-13 12:29:05 +0300

    Restructure the layout add favicon ico

---
+++ layouts/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Layout = () => {
+    return (
+      <>
+        <nav className="Nav bar here"></nav>
+        <main className="">Main components of Layout here</main>
+        <footer className="">Footer here</footer>
+      </>
+    );
+}
+
+export default Layout
